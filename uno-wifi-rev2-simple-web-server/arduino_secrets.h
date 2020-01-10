e21adab716f1f54b57e34f51f18cbe0163d52449
fudro <fudro1@gmail.com>
2020-01-10 11:15:49 -0800

    added debug output to udp sketch. Added simple server sketch.

---
+++ uno-wifi-rev2-simple-web-server/arduino_secrets.h
@@ -0,0 +1,3 @@
+char ssid[] = "NETGEAR12";        // your network SSID (name)
+char pass[] = "smilinghat005";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                 // your network key Index number (needed only for WEP)
